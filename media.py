86286d95b5faf7bf6f9a0356856b6026767136dc
Arvind <arvindrathee1996@gmail.com>
2017-07-10 22:04:32 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,19 +2,20 @@
 import webbrowser
 
 
-class Movie():
+class Movie(object):
     """This class proviedes a way to store movie related information
 
         Attributes:
-        
+
             title: A string to store the title of the movie.
             storyline: A string to store the basic plot of the movie.
             poster_image_url: A string to store the URL of the movie poster.
-            trailer_youtube_url: A string to store the URL of the movie trailer.
+            trailer_youtube_url: A string to store the URL of movie trailer.
             release_date: A string to store the release date of the movie.
     """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         """Initialises Movie class instance variables."""
         self.title = movie_title
         self.storyline = movie_storyline
